8295e430525fcc03d085f9ab7e10940b5c7e2822
Chip Kent <chipkent@deephaven.io>
2022-02-02 14:25:08 -0700

    Cleaned up sphinx env setup

--- sphinx/lib/dh_sphinx.py
+++ sphinx/lib/dh_sphinx.py
@@ -27,6 +27,14 @@ def setup_sphinx_environment():
     def exit_handler():
         print("Exit handler")
         jpy.destroy_jvm()
+
+        import sys, traceback, threading
+        thread_names = {t.ident: t.name for t in threading.enumerate()}
+        for thread_id, frame in sys._current_frames().iteritems():
+            print("Thread %s:" % thread_names.get(thread_id, thread_id))
+            traceback.print_stack(frame)
+            print()
+
         sys.exit()
 
     global _exit_timer
