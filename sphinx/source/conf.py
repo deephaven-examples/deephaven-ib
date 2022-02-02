2eb8d5db5066cc15798860510a22f3d9356515e0
Chip Kent <chipkent@deephaven.io>
2022-02-02 12:50:10 -0700

    Creating a jvm in sphinx

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -81,9 +81,53 @@ sys.path.append(str(new_python_path))
 # import jpy
 #
 # jpy.create_jvm(['-Xmx512M'])
+
+# from deephaven.start_jvm import start_jvm
+# start_jvm()
+
+# adapted from deephaven2/_utils/bootstrap.py
+
 from deephaven.start_jvm import start_jvm
 
-start_jvm()
+DEFAULT_DEVROOT = os.environ.get('DEEPHAVEN_DEVROOT', "/tmp/pyintegration")
+DEFAULT_WORKSPACE = os.environ.get('DEEPHAVEN_WORKSPACE', "/tmp")
+DEFAULT_PROPFILE = os.environ.get('DEEPHAVEN_PROPFILE', 'dh-defaults.prop')
+DEFAULT_CLASSPATH = os.environ.get('DEEPHAVEN_CLASSPATH', "/opt/deephaven/server/lib/*")
+
+
+def build_py_session():
+    if not jpy.has_jvm():
+        os.environ['JAVA_VERSION'] = '11'
+
+        # we will try to initialize the jvm
+        kwargs = {
+            'workspace': DEFAULT_WORKSPACE,
+            'devroot': DEFAULT_DEVROOT,
+            'verbose': False,
+            'propfile': DEFAULT_PROPFILE,
+            'java_home': os.environ.get('JDK_HOME', None),
+            'jvm_properties': {'PyObject.cleanup_on_thread': 'false'},
+            'jvm_options': {'-Djava.awt.headless=true',
+                            # '-Xms1g',
+                            # '-Xmn512m',
+                            '-XX:+UseG1GC',
+                            '-XX:MaxGCPauseMillis=100',
+                            '-XX:+UseStringDeduplication',
+                            '-XX:InitialRAMPercentage=25.0',
+                            '-XX:MinRAMPercentage=70.0',
+                            '-XX:MaxRAMPercentage=80.0',
+                            # '-XshowSettings:vm',
+                            # '-verbose:gc', '-XX:+PrintGCDetails',
+                            },
+            # 'jvm_maxmem': '1g',
+            'jvm_classpath': DEFAULT_CLASSPATH,
+            'skip_default_classpath': True
+        }
+        # initialize the jvm
+        start_jvm(**kwargs)
+
+
+build_py_session()
 
 
 import deephaven_ib
