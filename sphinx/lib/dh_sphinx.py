29ffa302ef9d634cafd5b8c98eef92b30dd554c0
Chip Kent <chipkent@deephaven.io>
2022-02-02 14:30:58 -0700

    Cleaned up sphinx env setup

--- sphinx/lib/dh_sphinx.py
+++ sphinx/lib/dh_sphinx.py
@@ -30,7 +30,7 @@ def setup_sphinx_environment():
 
         import sys, traceback, threading
         thread_names = {t.ident: t.name for t in threading.enumerate()}
-        for thread_id, frame in sys._current_frames().iteritems():
+        for thread_id, frame in sys._current_frames().items():
             print("Thread %s:" % thread_names.get(thread_id, thread_id))
             traceback.print_stack(frame)
             print()
