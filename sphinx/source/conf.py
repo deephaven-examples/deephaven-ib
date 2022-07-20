d0f0a4df19abf7c0278e4114782894b9626ef785
Chip Kent <chipkent@deephaven.io>
2022-07-20 12:56:52 -0600

    added deephaven_server to docs.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -82,6 +82,7 @@ import dh_sphinx
 
 dh_sphinx.setup_sphinx_environment()
 
+import deephaven_server
 import deephaven_ib
 import deephaven
 import ibapi.order
@@ -89,7 +90,7 @@ import ibapi.contract
 import jpy
 
 docs_title = "deephaven_ib python modules."
-package_roots = [deephaven_ib, deephaven, jpy, ibapi.contract, ibapi.order]
+package_roots = [deephaven_server, deephaven_ib, deephaven, jpy, ibapi.contract, ibapi.order]
 package_excludes = ['._']
 
 dh_sphinx.gen_sphinx_modules(docs_title, package_roots, package_excludes)
