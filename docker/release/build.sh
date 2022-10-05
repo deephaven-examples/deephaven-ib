765595b55b7c000ca5e90aab096263e45ffd4bcd
Chip Kent <chipkent@deephaven.io>
2022-10-05 13:53:39 -0600

    Cleaned up launch options and related documentation.

--- docker/release/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Build a docker image from released python packages
-#
-
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd ${__dir}
-
-docker build --build-arg IB_VERSION=1016.01 -t deephaven-examples/deephaven-ib:release -f Dockerfile .
