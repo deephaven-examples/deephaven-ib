e7c5211f5ca4d697d558097f4c359c8c12136494
Chip Kent <chipkent@deephaven.io>
2022-10-07 12:27:02 -0600

    Market making script udpate

--- examples/example_market_maker.py
+++ examples/example_market_maker.py
@@ -7,6 +7,7 @@ from deephaven.updateby import ema_time_decay
 from deephaven import time_table
 from deephaven.plot import Figure
 from deephaven.plot.selectable_dataset import one_click
+from deephaven.plot import PlotStyle
 
 
 ###########################################################################
@@ -28,10 +29,15 @@ print(f"IsConnected: {client.is_connected()}")
 
 account = "DU4943848"
 max_position_dollars = 10000.0
-ticks_bid_ask = client.tables["ticks_bid_ask"]
-orders_submitted = client.tables["orders_submitted"]
-orders_status = client.tables["orders_status"]
+ema_time = "00:02:00"
+
+errors = client.tables["errors"]
+requests = client.tables["requests"]
 positions = client.tables["accounts_positions"].where("Account = account")
+ticks_bid_ask = client.tables["ticks_bid_ask"]
+orders_submitted = client.tables["orders_submitted"].where("Account = account")
+orders_status = client.tables["orders_status"]
+orders_exec_details = client.tables["orders_exec_details"].where("Account = account")
 
 print("==============================================================================================================")
 print("==== Request data.")
@@ -40,7 +46,13 @@ print("=========================================================================
 registered_contracts_data = {}
 registred_contracts_orders = {}
 
-def add_contract(symbol, exchange="SMART"):
+def add_contract(symbol: str, exchange: str="SMART") -> None:
+    """
+    Configure a contract for trading.
+    :param symbol: Symbol to trade.
+    :param exchange: exchange where orders get routed.
+    :return: None
+    """
 
     contract = Contract()
     contract.symbol = symbol
@@ -71,7 +83,7 @@ print("=========================================================================
 
 preds = ticks_bid_ask \
     .update_view(["MidPrice=0.5*(BidPrice+AskPrice)", "MidPrice2=MidPrice*MidPrice"]) \
-    .update_by([ema_time_decay("Timestamp", "00:02:00", ["PredPrice=MidPrice","MidPrice2Bar=MidPrice2"])], by="Symbol") \
+    .update_by([ema_time_decay("Timestamp", ema_time, ["PredPrice=MidPrice","MidPrice2Bar=MidPrice2"])], by="Symbol") \
     .view([
         "ReceiveTime",
         "Timestamp",
@@ -86,6 +98,7 @@ preds = ticks_bid_ask \
         "PredHigh=PredPrice+PredSD",
     ])
 
+preds = preds.natural_join(preds.first_by("Symbol"), on="Symbol", joins="TimestampFirst=Timestamp")
 
 preds_one_click = one_click(preds, by=["Symbol"], require_all_filters=True)
 
@@ -105,6 +118,16 @@ print("=========================================================================
 open_orders = {}
 
 def update_orders(contract_id: int, pred_low: float, pred_high: float, buy_order: bool, sell_order:bool) -> int:
+    """
+    Update orders on a contract.  First existing orders are canceled.  Then new buy/sell limit orders are placed.
+
+    :param contract_id: Contract id.
+    :param pred_low: Price for buy limit orders.
+    :param pred_high: Price for sell limit orders.
+    :param buy_order: True to post a buy order; False to not post a buy order.
+    :param sell_order: True to post a sell order; False to not post a sell order.
+    :return: Number of orders submitted.
+    """
     print(f"START: update_orders: contract_id={contract_id}")
 
     if contract_id in open_orders:
@@ -150,21 +173,10 @@ def update_orders(contract_id: int, pred_low: float, pred_high: float, buy_order
     return len(new_orders)
 
 
-#TODO replace after bug fix
-# orders = time_table("00:01:00") \
-#     .rename_columns("SnapTime=Timestamp") \
-#     .snapshot(preds.last_by(["Symbol"])) \
-#     .natural_join(positions, on="ContractId", joins="Position") \
-#     .update_view([
-#         "Position = replaceIfNull(Position, 0.0)",
-#         "PositionDollars = Position * MidPrice",
-#         "MaxPositionDollars = max_position_dollars",
-#         "BuyOrder = PositionDollars < MaxPositionDollars",
-#         "SellOrder = PositionDollars > -MaxPositionDollars",
-#     ]) \
-#     .update("NumNewOrders = (long)update_orders(ContractId, PredLow, PredHigh, BuyOrder, SellOrder)")
-
-orders = preds.last_by(["Symbol"]) \
+orders = time_table("00:01:00") \
+    .rename_columns("SnapTime=Timestamp") \
+    .snapshot(preds.last_by(["Symbol"])) \
+    .where(f"Timestamp > TimestampFirst + '{ema_time}'") \
     .natural_join(positions, on="ContractId", joins="Position") \
     .update_view([
         "Position = replaceIfNull(Position, 0.0)",
@@ -172,12 +184,32 @@ orders = preds.last_by(["Symbol"]) \
         "MaxPositionDollars = max_position_dollars",
         "BuyOrder = PositionDollars < MaxPositionDollars",
         "SellOrder = PositionDollars > -MaxPositionDollars",
-    ])
-
-
-orders = time_table("00:01:00") \
-    .rename_columns("SnapTime=Timestamp") \
-    .snapshot(orders) \
+    ]) \
     .update("NumNewOrders = (long)update_orders(ContractId, PredLow, PredHigh, BuyOrder, SellOrder)")
 
 
+print("==============================================================================================================")
+print("==== Plot trade executions.")
+print("==============================================================================================================")
+
+
+trades = orders_exec_details \
+    .natural_join(preds.first_by("Symbol"), on="Symbol", joins="TimestampFirst=Timestamp") \
+    .where("ReceiveTime >= TimestampFirst") \
+    .view(["Timestamp=ReceiveTime", "ContractId", "Symbol", "ExecutionExchange", "Side", "Shares", "Price"])
+
+buys_one_click = one_click(trades.where("Side=`BOT`"), by=["Symbol"], require_all_filters=True)
+sells_one_click = one_click(trades.where("Side=`SLD`"), by=["Symbol"], require_all_filters=True)
+
+execution_plot = Figure() \
+    .plot_xy("BidPrice", t=preds_one_click, x="Timestamp", y="BidPrice") \
+    .plot_xy("AskPrice", t=preds_one_click, x="Timestamp", y="AskPrice") \
+    .plot_xy("MidPrice", t=preds_one_click, x="Timestamp", y="MidPrice") \
+    .plot_xy("PredPrice", t=preds_one_click, x="Timestamp", y="PredPrice") \
+    .plot_xy("PredLow", t=preds_one_click, x="Timestamp", y="PredLow") \
+    .plot_xy("PredHigh", t=preds_one_click, x="Timestamp", y="PredHigh") \
+    .twin() \
+    .axes(plot_style=PlotStyle.SCATTER) \
+    .plot_xy("Buys", t=buys_one_click, x="Timestamp", y="Price") \
+    .plot_xy("Sells", t=sells_one_click, x="Timestamp", y="Price") \
+    .show()
